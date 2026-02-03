4c242e0183025401edcd620d137ee9f6d2aa50d4
Kazui Miwa <kazui.miwa.0303@gmail.com>
2026-02-04 06:07:04 +0900

    fix

--- src/components/Timetable.tsx
+++ src/components/Timetable.tsx
@@ -50,7 +50,7 @@ const timetableData: TimetableItem[] = [
         title: "タイトル",
         speaker: "情報科○部○組 ○○",
         details: {
-          description: "ここに詳細を入れる",
+          description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
         }
       },
       {
