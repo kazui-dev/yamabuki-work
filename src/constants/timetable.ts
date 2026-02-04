3a8e8d4610cb128c4cf689d684c7b020934565e7
Kazui Miwa <kazui.miwa.0303@gmail.com>
2026-02-04 09:49:49 +0900

    fix

---
+++ src/constants/timetable.ts
@@ -0,0 +1,74 @@
+
+import image1 from "@/assets/images/image1.jpg";
+
+type TimetableItem = {
+  title: string;
+  time?: string;
+  speaker?: string;
+  description?: string;
+  details?: {
+    description: string;
+    image?: ImageMetadata;
+  };
+  sessions?: TimetableItem[];
+  action?: {
+    label: string;
+    url: string;
+  };
+};
+
+export const timetable: TimetableItem[] = [
+  {
+    time: "12:30",
+    title: "開会式",
+  },
+  {
+    time: "12:40",
+    title: "全体発表",
+    description: "ここに説明を入れる",
+    sessions: [
+      {
+        title: "タイトル",
+        speaker: "○○チーム",
+        details: {
+          description: "東京都立新宿山吹高等学校（とうきょうとりつ しんじゅく やまぶき こうとうがっこう、英: Shinjuku Yamabuki High School）は、東京都新宿区山吹町に所在する東京都立高等学校。",
+          image: image1
+        }
+      },
+      {
+        title: "タイトル",
+        speaker: "情報科○部○組 ○○",
+        details: {
+          description: "lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        }
+      },
+      {
+        title: "タイトル",
+        speaker: "情報科○部○組 ○○",
+        details: {
+          description: "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。",
+        }
+      },
+      {
+        title: "タイトル",
+        speaker: "情報科○部○組 ○○",
+        details: {
+          description: "ここに詳細を入れる",
+        }
+      }
+    ]
+  },
+  {
+    time: "14:00",
+    title: "ポスター発表",
+    description: "ここに説明を入れる",
+    action: {
+      label: "フロアマップ（未実装）",
+      url: "/"
+    }
+  },
+  {
+    time: "14:50",
+    title: "閉会式",
+  }
+];
