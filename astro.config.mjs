f5573a27804f610fd336796b0e814a94176b85d3
Kazui Miwa <kazui.miwa.0303@gmail.com>
2026-02-18 13:17:13 +0900

    fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,13 @@ import react from '@astrojs/react';
 import tailwindcss from '@tailwindcss/vite';
 import cloudflare from '@astrojs/cloudflare';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://yamabuki.work/',
   
-  integrations: [react()],
+  integrations: [react(), sitemap()],
 
   vite: {
     plugins: [tailwindcss()]
